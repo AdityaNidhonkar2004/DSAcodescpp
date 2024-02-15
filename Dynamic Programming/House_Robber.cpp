5516a0fb2b97e033a286dd302dc4b3fb397772e8
Aditya Nidhonkar <adityanidhonkar2004@gmail.com>
2024-02-15 19:53:34 +0530

    updated

--- Dynamic Programming/House_Robber.cpp
+++ Dynamic Programming/House_Robber.cpp
@@ -23,65 +23,65 @@
 
 // Solution:
 // Approach:
-- Here we we apply recurrsion and whe n we are at ith house we will either rob it and jump to(i + 2)th house OR we won't rob the ith house and jump to (i+1)th house  - Recurrance relation:
-    i->index to iterate, arr->array
+// - Here we we apply recurrsion and whe n we are at ith house we will either rob it and jump to(i + 2)th house OR we won't rob the ith house and jump to (i+1)th house  - Recurrance relation:
+//     i->index to iterate, arr->array
 
-                             | -- --->arr[i] + f(arr, i + 2); // rob and jump by 2
-f(arr, i)-- -- |
-    | -- --->0 + f(arr, i); // don't rob and jump by 1
-so ans is maximum of above both conditions - Base case will be if i exceeds the size of arr then return 0;
+//                        |----->arr[i] + f(arr, i + 2); // rob and jump by 2
+// f(arr, i)----maximum)of|
+//                        |----->0 + f(arr, i); // don't rob and jump by 1
+// so ans is maximum of above both conditions - Base case will be if i exceeds the size of arr then return 0;
 
-    // Code
-    class Solution{
-      public:
-vector<int> dp;
-// Recurrsive
-int fr(vector<int> v, int i)
+// Code
+class Solution
 {
-  if (i == v.size() - 1)
-    return v[i];
-  if (i == v.size() - 2)
-    return max(v[i], v[i + 1]);
-
-  return max(v[i] + fr(v, i + 2), 0 + fr(v, i + 1));
-}
-
-// top down- dp
-int ftd(vector<int> v, int i)
-{
-  if (i == v.size() - 1)
-    return v[i];
-  if (i == v.size() - 2)
-    return max(v[i], v[i + 1]);
-  if (dp[i] != -1)
-    return dp[i];
-  return dp[i] = max(v[i] + ftd(v, i + 2), ftd(v, i + 1));
-}
-int fbu(vector<int> v)
-{
-  int n = v.size();
-  if (n == 1)
-    return v[0];
+public:
   vector<int> dp;
-  dp.clear();
-  dp.resize(n, 0);
-  dp[n - 1] = v[n - 1];
-  dp[n - 2] = max(v[n - 1], v[n - 2]);
-  for (int i = n - 3; i >= 0; i--)
+  // Recurrsive
+  int fr(vector<int> v, int i)
   {
-    dp[i] = max(v[i] + dp[i + 2], 0 + dp[i + 1]);
-  }
-  return dp[0];
-}
+    if (i == v.size() - 1)
+      return v[i];
+    if (i == v.size() - 2)
+      return max(v[i], v[i + 1]);
 
-int rob(vector<int> &nums)
-{
-  dp.clear();
-  dp.resize(105, -1); // resize it to slightly more than the given range for safety
-  int i = 0;
-  // return fr(nums,i);
-  // return fdp(nums,i);
-  return fbu(nums);
-}
-}
-;
+    return max(v[i] + fr(v, i + 2), 0 + fr(v, i + 1));
+  }
+
+  // top down- dp
+  int ftd(vector<int> v, int i)
+  {
+    if (i == v.size() - 1)
+      return v[i];
+    if (i == v.size() - 2)
+      return max(v[i], v[i + 1]);
+    if (dp[i] != -1)
+      return dp[i];
+    return dp[i] = max(v[i] + ftd(v, i + 2), ftd(v, i + 1));
+  }
+  int fbu(vector<int> v)
+  {
+    int n = v.size();
+    if (n == 1)
+      return v[0];
+    vector<int> dp;
+    dp.clear();
+    dp.resize(n, 0);
+    dp[n - 1] = v[n - 1];
+    dp[n - 2] = max(v[n - 1], v[n - 2]);
+    for (int i = n - 3; i >= 0; i--)
+    {
+      dp[i] = max(v[i] + dp[i + 2], 0 + dp[i + 1]);
+    }
+    return dp[0];
+  }
+
+  int rob(vector<int> &nums)
+  {
+    dp.clear();
+    dp.resize(105, -1); // resize it to slightly more than the given range for safety
+    int i = 0;
+    // return fr(nums,i);
+    // return fdp(nums,i);
+    return fbu(nums);
+  }
+};
